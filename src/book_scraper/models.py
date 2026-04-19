1be8815e901b23291d69694ee172f8937dac131e
martinbolger <56566763+martinbolger@users.noreply.github.com>
2026-04-20 08:29:38 +0900

    Add database model for a BookEntry

--- src/book_scraper/models.py
+++ src/book_scraper/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, Boolean, Float
 from sqlalchemy.orm import DeclarativeBase
 from datetime import datetime
 
@@ -11,3 +11,18 @@ class ManifestEntry(Base):
     __tablename__ = "manifest"
     url = Column(String, primary_key=True)
     last_read_date = Column(DateTime, default=datetime.now)
+
+
+class BookEntry(Base):
+    __tablename__ = "books"
+    listingid = Column(String, primary_key=True)
+    image_url = Column(String)
+    title = Column(String)
+    magazine = Column(Boolean)
+    sold_date = Column(DateTime)
+    price_usd = Column(Float)
+    shipping_cost = Column(Float)
+    total_price = Column(Float)
+    resolution_attempted = Column(Boolean, default=False)
+    title_jp = Column(String)
+    isbn = Column(String)
